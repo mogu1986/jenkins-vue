b1fba569618449757fb0e6705f451e0f52735188
gaowei <gaowei@fengjinggroup.com>
2019-06-20 09:57:07 +0800

    artifact

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App 英雄的黎明!"/>
+    <HelloWorld msg="Welcome to Your Vue.js App 黎明的英雄!"/>
   </div>
 </template>
 
